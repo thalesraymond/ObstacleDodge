27f6c6c989f339b46dfaffa270dd0c37c0e5a115
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-01-28 23:30:21 -0300

    adding walls

--- Assets/Scripts/PlayerStates/PlayerMoveState.cs
+++ Assets/Scripts/PlayerStates/PlayerMoveState.cs
@@ -16,12 +16,10 @@ namespace PlayerStates
         public override void Update()
         {
             base.Update();
-            
-            var airVelocity = this.Player.Rigidbody.linearVelocity.y;
 
             var velocityMultiplier = (this.Player.MovementSpeed * Time.deltaTime);
             
-            var inputVectorDirection = new Vector3(this.InputManager.MoveInput.x * velocityMultiplier, airVelocity, this.InputManager.MoveInput.y * velocityMultiplier);
+            var inputVectorDirection = new Vector3(this.InputManager.MoveInput.x * velocityMultiplier, 0, this.InputManager.MoveInput.y * velocityMultiplier);
             
             this.Player.Rigidbody.AddForce(inputVectorDirection);
             
